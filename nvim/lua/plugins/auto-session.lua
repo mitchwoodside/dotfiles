6de69d0653235cb820ffee38ed7f426668de4d6e
Mitch Woodside <carmanmitchellwoodside@gmail.com>
2025-04-11 23:18:50 -0400

    add auto-session

---
+++ nvim/lua/plugins/auto-session.lua
@@ -0,0 +1,12 @@
+return {
+  'rmagatti/auto-session',
+  lazy = false,
+
+  ---enables autocomplete for opts
+  ---@module "auto-session"
+  ---@type AutoSession.Config
+  opts = {
+    enabled = true,
+    -- log_level = 'debug',
+  }
+}
